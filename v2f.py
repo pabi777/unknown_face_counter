35a35350c3a9a50e4fab8e8a45e39303cc5359f7
Pabitra <roypapu2@gmail.com>
2019-06-20 22:03:43 +0530

    checkpoint

--- v2f.py
+++ v2f.py
@@ -5,24 +5,10 @@ from PIL import Image,ImageDraw
 import numpy as np
 from time import sleep
 
-def faces(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Detect faces in the image
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        #flags = cv2.CV_HAAR_SCALE_IMAGE
-    )
-    return faces
-
 
 cap = cv2.VideoCapture('nfs.mp4')
 unknown_count=0
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-framelist=[]
+
 while(cap.isOpened()):
     try:
         # Capture frame-by-frame
@@ -30,7 +16,6 @@ while(cap.isOpened()):
         rgb_frame = frame[:, :, ::-1]
         face_locations = face_recognition.face_locations(rgb_frame)
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-        face=faces(frame)
         if face_encodings is not None:
             ret1, frame1 = cap.read()
             rgb_frame1 = frame1[:, :, ::-1]
@@ -41,14 +26,13 @@ while(cap.isOpened()):
             if face_encodings1 is not None:
                 match = face_recognition.compare_faces(face_encodings, face_encodings1[0])
                 for match in match:
-                    
                     if not match:
+                        print(face_locations,face_locations1)
                         for (top, right, bottom, left) in face_locations:
                             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
                             cv2.imshow('frame', frame)
                             unknown_count+=1
                             print(unknown_count)
-
             else:
                 continue
         else:
